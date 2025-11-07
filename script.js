3ea3e41c1deb90d0f9ff232a386c91292b9eccb3
Maycon Douglas <hellomaycondouglas@gmail.com>
2025-11-07 09:28:04 -0300

    adiciona função que gera escolha da máquina

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+function getComputerChoice(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  const number = Math.floor(
+    Math.random() * (maxFloored - minCeiled + 1) + minCeiled
+  );
+
+  let computerChoice = "";
+
+  if (number == 1) {
+    computerChoice = "Pedra";
+  } else if (number == 2) {
+    computerChoice = "Papel";
+  } else {
+    computerChoice = "Tesoura";
+  }
+  return computerChoice;
+}
